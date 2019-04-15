5c14e6385901a19f1350a47f9bcb4625455c32df
jinruimeng <jinruimeng@163.com>
2019-04-15 16:26:21 +0800

    2019.04.15 16:26

--- main_cluster.py
+++ main_cluster.py
@@ -4,50 +4,61 @@ import getCovMatrix
 import pca
 import time
 import numpy as np
+import multiprocessing
+
+
+def cluster(type, path, suffix, channelData, g):
+    print("第" + str(g) + "轮开始！")
+    centroidListPath = path + "getCentroids_outCentroidList_" + type + "_" + str(g) + "_.xlsx"
+    centroidList = readAndWriteDataSet.excelToMatrixList(centroidListPath)
+
+    # 计算出信道数据的协方差
+    centroids = getCovMatrix.matrixListToMatrix(centroidList)
+    covMatrixList = getCovMatrix.getCovMatrixList(channelData)
+    allCovMatrix = getCovMatrix.matrixListToMatrix(covMatrixList)
+
+    # 确定每个数据分别属于哪个簇
+    clusterAssment = kmeans.getClusterAssment(allCovMatrix, centroids, type)
+    clusterAssmentList = []
+    clusterAssmentList.append(clusterAssment)
+
+    # 分析PCA效果
+    newChannelData, newCovMatrixs = pca.pca(channelData, covMatrixList, centroidList, clusterAssment, 0.8)
+
+    # 输出
+    nowTime = time.strftime("%Y-%m-%d.%H.%M.%S", time.localtime(time.time()))
+    outOldCovMatrixListPath = path + "cluster_outOldCovMatrixList_" + type + "_" + str(g) + "_" + str(
+        nowTime) + suffix
+    outClusterAssmentPath = path + "cluster_outClusterAssment_" + type + "_" + str(g) + "_" + str(nowTime) + suffix
+    outNewChannelDataPath = path + "cluster_outNewChannelData_" + type + "_" + str(g) + "_" + str(nowTime) + suffix
+    outNewCovMatrixsPath = path + "cluster_outNewCovMatrixList_" + type + "_" + str(g) + "_" + str(nowTime) + suffix
+    readAndWriteDataSet.write(covMatrixList, outOldCovMatrixListPath)
+    readAndWriteDataSet.write(clusterAssmentList, outClusterAssmentPath)
+    readAndWriteDataSet.write(newChannelData, outNewChannelDataPath)
+    readAndWriteDataSet.write(newCovMatrixs, outNewCovMatrixsPath)
+    print("第" + str(g) + "轮结束！")
+
 
 if __name__ == '__main__':
     type = "oushi"
     # path = "/Users/jinruimeng/Downloads/keyan/"
     path = "E:\\workspace\\keyan\\"
     suffix = ".xlsx"
-    channelDataPath = path + "channelData.xlsx"
+    channelDataPath = path + "channelData1.xlsx"
     channelDataAll = readAndWriteDataSet.excelToMatrixList(channelDataPath)
     n = np.shape(channelDataAll[0])[1]  # 列数
     p = len(channelDataAll)  # 页数
-    a = 4  # 拆分成2^a份
+    a = 1  # 拆分成2^a份
     sub = n >> a
+    ps = multiprocessing.Pool(8)
 
     for g in range(1 << a):
-        print("第" + str(g) + "轮开始！")
         channelData = []
         for i in range(p):
             channelDataPage = channelDataAll[i]
             channelData.append(channelDataPage[:, g * sub:(g + 1) * sub])
-        centroidListPath = path + "getCentroids_outCentroidList_" + type + "_" + str(g) + "_.xlsx"
-        centroidList = readAndWriteDataSet.excelToMatrixList(centroidListPath)
+        ps.apply_async(cluster, args=(type, path, suffix, channelData, g))
 
-        # 计算出信道数据的协方差
-        centroids = getCovMatrix.matrixListToMatrix(centroidList)
-        covMatrixList = getCovMatrix.getCovMatrixList(channelData)
-        allCovMatrix = getCovMatrix.matrixListToMatrix(covMatrixList)
-
-        # 确定每个数据分别属于哪个簇
-        clusterAssment = kmeans.getClusterAssment(allCovMatrix, centroids, type)
-        clusterAssmentList = []
-        clusterAssmentList.append(clusterAssment)
-
-        # 分析PCA效果
-        newChannelData, newCovMatrixs = pca.pca(channelData, covMatrixList, centroidList, clusterAssment, 0.8)
-
-        # 输出
-        nowTime = time.strftime("%Y-%m-%d.%H.%M.%S", time.localtime(time.time()))
-        outOldCovMatrixListPath = path + "cluster_outOldCovMatrixList_" + type + "_" + str(g) + "_" + str(
-            nowTime) + suffix
-        outClusterAssmentPath = path + "cluster_outClusterAssment_" + type + "_" + str(g) + "_" + str(nowTime) + suffix
-        outNewChannelDataPath = path + "cluster_outNewChannelData_" + type + "_" + str(g) + "_" + str(nowTime) + suffix
-        outNewCovMatrixsPath = path + "cluster_outNewCovMatrixList_" + type + "_" + str(g) + "_" + str(nowTime) + suffix
-        readAndWriteDataSet.write(covMatrixList, outOldCovMatrixListPath)
-        readAndWriteDataSet.write(clusterAssmentList, outClusterAssmentPath)
-        readAndWriteDataSet.write(newChannelData, outNewChannelDataPath)
-        readAndWriteDataSet.write(newCovMatrixs, outNewCovMatrixsPath)
-        print("第" + str(g) + "轮结束！")
+    ps.close()
+    ps.join()
+    print("主进程结束！")
--- main_getCentroids.py
+++ main_getCentroids.py
@@ -5,6 +5,49 @@ import pca
 import elbow
 import time
 import numpy as np
+import multiprocessing
+
+
+def getCentroids(type, path, suffix, channelData, g, k):
+    print("第" + str(g) + "轮开始！")
+    # 得到协方差的集合，并把协方差放到一个矩阵中
+    covMatrixList = getCovMatrix.getCovMatrixList(channelData)
+    allCovMatrix = getCovMatrix.matrixListToMatrix(covMatrixList)
+
+    # 利用肘部法选择聚类中心个数
+    # elbow.elbow(allCovMatrix, 1, 8, type)
+
+    # 对协方差进行聚类
+    if "mashi" == type:
+        centroids, clusterAssment = kmeans.KMeansMashi(allCovMatrix, k)
+    else:
+        centroids, clusterAssment = kmeans.KMeansOushi(allCovMatrix, k)
+
+    clusterAssmentList = []
+    clusterAssmentList.append(clusterAssment)
+    centroidList = getCovMatrix.matrixToMatrixList(centroids)
+
+    # 分析PCA效果
+    newChannelData, newCovMatrixList = pca.pca(channelData, covMatrixList, centroidList, clusterAssment, 0.8)
+
+    # 输出
+    nowTime = time.strftime("%Y-%m-%d.%H.%M.%S", time.localtime(time.time()))
+    outOldCovMatrixListPath = path + "getCentroids_outOldCovMatrixList_" + type + str(g) + str(
+        nowTime) + suffix
+    outCentroidListPath = path + "getCentroids_outCentroidList_" + type + "_" + str(g) + "_" + str(nowTime) + suffix
+    outClusterAssmentPath = path + "getCentroids_outClusterAssment_" + type + "_" + str(g) + "_" + str(
+        nowTime) + suffix
+    outNewChannelDataPath = path + "getCentroids_outNewChannelData_" + type + "_" + str(g) + "_" + str(
+        nowTime) + suffix
+    outNewCovMatrixListPath = path + "getCentroids_outNewCovMatrixList_" + type + "_" + str(g) + "_" + str(
+        nowTime) + suffix
+    readAndWriteDataSet.write(covMatrixList, outOldCovMatrixListPath)
+    readAndWriteDataSet.write(clusterAssmentList, outClusterAssmentPath)
+    readAndWriteDataSet.write(centroidList, outCentroidListPath)
+    readAndWriteDataSet.write(newChannelData, outNewChannelDataPath)
+    readAndWriteDataSet.write(newCovMatrixList, outNewCovMatrixListPath)
+    print("第" + str(g) + "轮结束！")
+
 
 if __name__ == '__main__':
     type = "oushi"
@@ -13,54 +56,22 @@ if __name__ == '__main__':
     suffix = ".xlsx"
 
     # 读取数据
-    channelDataPath = path + "channelData.xlsx"
+    channelDataPath = path + "channelData1.xlsx"
     channelDataAll = readAndWriteDataSet.excelToMatrixList(channelDataPath)
     n = np.shape(channelDataAll[0])[1]  # 列数
     p = len(channelDataAll)  # 页数
-    a = 4  # 拆分成2^a份
+    a = 1  # 拆分成2^a份
     sub = n >> a
+    k = 2  # 聚类中心数量
+    ps = multiprocessing.Pool(8)
 
     for g in range(1 << a):
-        print("第" + str(g) + "轮开始！")
         channelData = []
         for i in range(p):
             channelDataPage = channelDataAll[i]
             channelData.append(channelDataPage[:, g * sub:(g + 1) * sub])
-        # 得到协方差的集合，并把协方差放到一个矩阵中
-        covMatrixList = getCovMatrix.getCovMatrixList(channelData)
-        allCovMatrix = getCovMatrix.matrixListToMatrix(covMatrixList)
+        ps.apply_async(getCentroids, args=(type,path,suffix,channelData, g, k))
 
-        # 利用肘部法选择聚类中心个数
-        # elbow.elbow(allCovMatrix, 1, 8, type)
-
-        # 对协方差进行聚类
-        k = 10
-        if "mashi" == type:
-            centroids, clusterAssment = kmeans.KMeansMashi(allCovMatrix, k)
-        else:
-            centroids, clusterAssment = kmeans.KMeansOushi(allCovMatrix, k)
-
-        clusterAssmentList = []
-        clusterAssmentList.append(clusterAssment)
-        centroidList = getCovMatrix.matrixToMatrixList(centroids)
-
-        # 分析PCA效果
-        newChannelData, newCovMatrixList = pca.pca(channelData, covMatrixList, centroidList, clusterAssment, 0.8)
-
-        # 输出
-        nowTime = time.strftime("%Y-%m-%d.%H.%M.%S", time.localtime(time.time()))
-        outOldCovMatrixListPath = path + "getCentroids_outOldCovMatrixList_" + type + str(g) + str(
-            nowTime) + suffix
-        outCentroidListPath = path + "getCentroids_outCentroidList_" + type + "_" + str(g) + "_" + str(nowTime) + suffix
-        outClusterAssmentPath = path + "getCentroids_outClusterAssment_" + type + "_" + str(g) + "_" + str(
-            nowTime) + suffix
-        outNewChannelDataPath = path + "getCentroids_outNewChannelData_" + type + "_" + str(g) + "_" + str(
-            nowTime) + suffix
-        outNewCovMatrixListPath = path + "getCentroids_outNewCovMatrixList_" + type + "_" + str(g) + "_" + str(
-            nowTime) + suffix
-        readAndWriteDataSet.write(covMatrixList, outOldCovMatrixListPath)
-        readAndWriteDataSet.write(clusterAssmentList, outClusterAssmentPath)
-        readAndWriteDataSet.write(centroidList, outCentroidListPath)
-        readAndWriteDataSet.write(newChannelData, outNewChannelDataPath)
-        readAndWriteDataSet.write(newCovMatrixList, outNewCovMatrixListPath)
-        print("第" + str(g) + "轮结束！")
+    ps.close()
+    ps.join()
+    print("主进程结束！")
