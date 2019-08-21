a4c832e6b2bc2c4c7d6a31dbf75c060267590b7b
jinruimeng <jinruimeng@163.com>
2019-08-21 13:33:02 +0800

    C

--- kmeans.py
+++ kmeans.py
@@ -3,8 +3,9 @@ import numpy as np
 import getDistance
 import readAndWriteDataSet
 
+
 # 从数据中随机选择k个作为起始的中心点
-def randCent2(dataSetList, k):
+def randCent(dataSetList, k):
     p = np.shape(dataSetList)[0]  # 查看数据个数
     centroidList = []
     rs = random.sample(range(0, p), k)  # 在0~m范围内生成k个随机数[0,m)
@@ -14,9 +15,56 @@ def randCent2(dataSetList, k):
 
 
 # k均值聚类
-# 根据代价函数聚类
+# 根据欧式距离聚类 dataSet为协方差矩阵
+def KMeansOushi(dataSet, k):
+    m = np.shape(dataSet)[0]  # 行的数目
+    # 第一列存样本属于哪一簇
+    # 第二列存样本的到簇的中心点的误差
+    clusterAssment = np.matrix(np.ones((m, 2)))
+    clusterChange = True
+    count = 0
+
+    # 第1步 初始化centroids
+    centroids = randCent(dataSet, k)
+    while clusterChange:
+        count += 1
+        print("第" + str(count) + "次寻找聚类中心！")
+        clusterChange = False
+
+        # 遍历所有的样本（行数）
+        for i in range(m):
+            minDist = 100000000.0
+            minIndex = -1
+
+            # 遍历所有的质心
+            # 第2步 找出最近的质心
+            for j in range(k):
+                # 计算该样本到质心的欧式距离
+                distance = getDistance.oushiDistance(centroids[j, :], dataSet[i, :])
+                if distance < minDist:
+                    minDist = distance
+                    minIndex = j
+
+            # 第 3 步：更新每一行样本所属的簇
+            if clusterAssment[i, 0] != minIndex:
+                clusterChange = True
+                clusterAssment[i, :] = minIndex, minDist
+
+        # 第 4 步：更新质心
+        for j in range(k):
+            pointsInCluster = dataSet[np.nonzero(clusterAssment[:, 0].A == j)[0]]  # 获取簇类所有的点
+            if np.shape(pointsInCluster)[0] > 1:
+                centroids[j, :] = np.mean(pointsInCluster, axis=0)  # 对矩阵的行求均值
+        for i in range(m):
+            clusterAssment[i, 1] = getDistance.oushiDistance(centroids[int(clusterAssment[i, 0].real), :], dataSet[i, :])
+
+    print("Congratulations,cluster complete!")
+    return centroids, clusterAssment
+
+
+# 根据代价函数聚类 dataSet为信道信息矩阵
 # k-聚类中心数量 npower-噪声功率 newDimension-保留维度
-def KMeansOushi_costFunction(dataSet, k, npower, newDimension=5):
+def KMeansOushi_costFunction(dataSet, covs, k, npower, newDimension=5):
     m = np.shape(dataSet)[0]  # 样本数
     n = np.shape(dataSet)[2]  # 列数
     # 第一列存样本属于哪一簇
@@ -26,14 +74,12 @@ def KMeansOushi_costFunction(dataSet, k, npower, newDimension=5):
     count = 0
 
     maxIs = []
-    covs = []
     for i in range(m):
         maxI = getDistance.costFunction(dataSet[i], np.eye(n), npower[i])
         maxIs.append(maxI)
-        covs.append(np.cov(dataSet[i], rowvar=False))
 
     # 第1步 初始化centroids
-    centroidList = randCent2(covs, k)
+    centroidList = randCent(covs, k)
     UList = []
     for i in range(k):
         U, Sigma, VT = np.linalg.svd(centroidList[i])
@@ -84,6 +130,7 @@ def KMeansOushi_costFunction(dataSet, k, npower, newDimension=5):
 
     print("Congratulations,cluster complete!")
     return centroidList, UList, clusterAssment
+
 
 if __name__ == '__main__':
     datafile = u'E:\\workspace\\keyan\\test.xlsx'
--- main.py
+++ main.py
@@ -10,25 +10,27 @@ import pca
 import wt
 
 
-def cluster(channelData_a, channelData_b, UList, clusterAssment):
+def cluster(channelData_a, channelData_b, k, UList_C, clusterAssment_C, UList_F, clusterAssment_F):
     # 无交互PCA
-    newPca1, newPca2 = clusterCore(channelData_a, channelData_b, UList, clusterAssment, "general")
+    newPca1, newPca2 = clusterCore(channelData_a, channelData_b, k, [], [], "general")
 
     # 聚类协方差矩阵
-    newC1, newC2 = clusterCore(channelData_a, channelData_b, UList, clusterAssment, "C")
+    newC1, newC2 = clusterCore(channelData_a, channelData_b, k, UList_C, clusterAssment_C, "C")
+
+    # 聚类协方差矩阵_代价函数
+    newF1, newF2 = clusterCore(channelData_a, channelData_b, k, UList_F, clusterAssment_F, "F")
 
     # wt变换
-    newWt1, newWt2 = clusterCore(channelData_a, channelData_b, UList, clusterAssment, "wt")
+    newWt1, newWt2 = clusterCore(channelData_a, channelData_b, k, [], [], "wt")
 
-    return newPca1, newPca2, newC1, newC2, newWt1, newWt2
+    return newPca1, newPca2, newC1, newC2, newF1, newF2, newWt1, newWt2
 
 
-def clusterCore(channelData_a, channelData_b, UList, clusterAssment, type):
+def clusterCore(channelData_a, channelData_b, newDimension, UList, clusterAssment, type):
     newChannelData1 = []
     newChannelData2 = []
-    newDimension = np.shape(UList[0])[1]
 
-    if type == "C":
+    if type == "C" or type == "F":
         # 变换域
         for i in range(np.shape(channelData_a)[0]):
             newChannelData1.append(np.dot(channelData_a[i], UList[(int)(clusterAssment[i, 0].real)]))
@@ -79,22 +81,36 @@ if __name__ == '__main__':
         print(u'降维后维度不能小于1！')
         sys.exit()
 
-    lengths = np.zeros((3, p))
-    errorNums = np.zeros((3, p))
+    lengths = np.zeros((4, p))
+    errorNums = np.zeros((4, p))
     newSNRs_newPca = []
     newSNRs_newC = []
+    newSNRs_newF = []
     newSNRs_newWt = []
 
+    covs = tools.getCovMatrixList(channelData_a)
+    covs2 = []
+    for i in range(p):
+        covs2.append(covs[i] - np.eye(n) * npowers[i])
     # 聚类
-    centroidList, UList, clusterAssment = kmeans.KMeansOushi_costFunction(channelData_a, k, npowers, iRate)
+    centroids_C, clusterAssment_C = kmeans.KMeansOushi(tools.matrixListToMatrix(covs2), k)
+    centroidList_C = tools.matrixToMatrixList(centroids_C)
+    UList_C = []
+    for i in range(k):
+        U, Sigma, VT = np.linalg.svd(centroidList_C[i])
+        UList_C.append(np.transpose(U)[:, 0:iRate])
+
+    centroidList_F, UList_F, clusterAssment_F = kmeans.KMeansOushi_costFunction(channelData_a, covs, k, npowers, iRate)
 
     # 预处理
-    newPca1, newPca2, newC1, newC2, newWt1, newWt2 = cluster(channelData_a, channelData_b, UList, clusterAssment)
+    newPca1, newPca2, newC1, newC2, newF1, newF2, newWt1, newWt2 = cluster(channelData_a, channelData_b, k, UList_C, clusterAssment_C, UList_F, clusterAssment_F)
 
     key_newPca1 = []
     key_newPca2 = []
     key_newC1 = []
     key_newC2 = []
+    key_newF1 = []
+    key_newF2 = []
     key_newWt1 = []
     key_newWt2 = []
 
@@ -115,35 +131,31 @@ if __name__ == '__main__':
         errorNums[1, i] += errorNum
         newSNRs_newC.append(SNRList)
 
+        tmpKey1, tmpKey2, SNRList = quantification.quantificate(newF1[i], newF2[i], npowers[i])
+        key_newC1.append(tmpKey1)
+        key_newC2.append(tmpKey2)
+        keyLength, errorNum = quantification.getInconsistencyRate(tmpKey1, tmpKey2)
+        lengths[2, i] += keyLength
+        errorNums[2, i] += errorNum
+        newSNRs_newF.append(SNRList)
+
         tmpKey1, tmpKey2, SNRList = quantification.quantificate(newWt1[i], newWt2[i], npowers[i])
         key_newWt1.append(tmpKey1)
         key_newWt2.append(tmpKey2)
         keyLength, errorNum = quantification.getInconsistencyRate(tmpKey1, tmpKey2)
-        lengths[2, i] += keyLength
-        errorNums[2, i] += errorNum
+        lengths[3, i] += keyLength
+        errorNums[3, i] += errorNum
         newSNRs_newWt.append(SNRList)
 
     readAndWriteDataSet.writeKey(path, key_newPca1, key_newPca2, SNR, u'pca')
     readAndWriteDataSet.writeKey(path, key_newC1, key_newC2, SNR, u'C')
+    readAndWriteDataSet.writeKey(path, key_newF1, key_newF2, SNR, u'F')
     readAndWriteDataSet.writeKey(path, key_newWt1, key_newWt2, SNR, u'wt')
-
-    # 计算信息熵
-    # total_key_pca = u''
-    # total_key_C = u''
-    # total_key_wt = u''
-    # for i in range(np.shape(key_newPca1)[0]):
-    #     total_key_pca = total_key_pca + key_newPca1[i] + key_newPca2[i]
-    #     total_key_C = total_key_C + key_newC1[i] + key_newC2[i]
-    #     total_key_wt = total_key_wt + key_newWt1[i] + key_newWt2[i]
-    #
-    # ents[0, h] = quantification.calc_shannon_ent(total_key_pca)
-    # ents[1, h] = quantification.calc_shannon_ent(total_key_C)
-    # ents[2, h] = quantification.calc_shannon_ent(total_key_wt)
 
     readAndWriteDataSet.write(tools.listToArray(newSNRs_newPca), path + u'newSNRs_newPca')
     readAndWriteDataSet.write(tools.listToArray(newSNRs_newC), path + u'newSNRs_newC')
+    readAndWriteDataSet.write(tools.listToArray(newSNRs_newF), path + u'newSNRs_newF')
     readAndWriteDataSet.write(tools.listToArray(newSNRs_newWt), path + u'newSNRs_newWt')
     readAndWriteDataSet.write(lengths, path + u'length')
     readAndWriteDataSet.write(errorNums, path + u'errorNum')
-    # readAndWriteDataSet.write(ents, path + u'ent')
     print("主进程结束！")
